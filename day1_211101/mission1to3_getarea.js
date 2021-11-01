b70247ffc840ac3842c3b12549fdfee83bfea800
bangdler <zbthz90@gmail.com>
2021-11-01 22:02:49 +0900

    add : day1. mission 1/2/3

---
+++ day1_211101/mission1to3_getarea.js
@@ -0,0 +1,48 @@
+console.log("hello world");
+
+var logging = [];
+var results = [];
+
+function getArea(figure, ...params) {
+    // a = a || 1;
+    // b = b || 1;
+    // c = c || 1;
+    logging.push(figure)
+    if (figure == 'circle') {
+        var getCircle = function(a) {
+            return (a ** 2) * Math.PI;
+        }
+        var area = 0;
+        var radius = params[0];
+        var iters = params[1] || 1;
+        for (var i = 0; i < iters; i++) {
+            area = area + getCircle(radius + i);
+        }
+    }
+    else if (figure == 'rect') {
+        var height = params[0];
+        var width = params[1];
+        var area = height * width
+    }
+    else if (figure == 'trapezoid') {
+        var height = params[0];
+        var upper_width = params[1];
+        var lower_width = params[2];
+        var area = height * (upper_width + lower_width);
+    }
+    results.push(area);
+    console.log(area)
+    return area;
+}
+
+function printExecutionSequence() {
+    console.log('계산수행순서 :', logging.toString());
+    console.log('결과값순서 :', results.toString());
+}
+
+getArea('trapezoid', 10, 2, 4);
+getArea('circle', 5, 2);
+getArea('rect', 10, 1);
+getArea('circle', 10);
+
+printExecutionSequence();
---
+++ study.md
@@ -0,0 +1,15 @@
+# Bandler's study note
+## Day 1 (2021-11-01)
+
+Mission 1
+1. 도형 종류에 대한 인자 외에 경우에 따라 1~3개 인자가 input 으로 들어올 때, a, b, c 지정 후 인자가 필요 없는 경우를 대비하여 기본값을 1로 지정하였음. (a = a || 1)
+2. 이 경우 d, e 등 추가 인자가 들어올 경우 error가 발생할 수 있으므로 ...params 를 통해 여러 인자를 받을 수 있도록 수정하여 list 형식으로 받음.
+3. getCircle 함수 안에 2개 인자가 있으나 두번째 인자가 input 되지 않는 경우가 있으므로 기본값 지정이 필요함. (b = params[1] || 1) 
+4. getCircle 함수 안에 return을 적지 않아 결과값으로 undefined 나옴.
+5. for 문을 활용한 sum 구하기
+   var sum_area = 0;
+   var iters = params[1] || 1;
+   for (var i = 0; i < iters; i++) {
+       sum_area = sum_area + getCircle(radius + i)
+   }
+   
