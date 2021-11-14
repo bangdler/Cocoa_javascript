a844346401ca5fad269850b72c2c616205a63fe2
bangdler <zbthz90@gmail.com>
2021-11-14 19:19:56 +0900

    add : mission - 숫자타입인 구성요소 배열 만들기

---
+++ Week1/Mission_wed/mission_makeTypeNumber.js
@@ -0,0 +1,32 @@
+// o.js 모듈에서 data 를 import
+import {data} from './o.js'
+
+//연습용 객체
+let data2 =  {
+    "debug" : 200,
+    "window" : {
+        "title": 'Sample Konfabulator Widget',
+        name: 'main_window',
+        width: 500,
+        height: 500
+}
+}
+console.log(typeof(data2))
+
+
+let results = [];
+
+// for in 을 사용하여 객체의 value type 이 Number 이면 result 배열에 그 Key 값을 넣는다. value가 객체로 구성되어 있으면 함수를 다시 적용한다.
+function getTypeNumber(data) {
+    for (let key in data) {
+        if (typeof(data[key]) === "number") {
+            results.push(key);
+        }
+        else if (typeof(data[key]) === "object") {
+            getTypeNumber(data[key])
+        }
+    }
+    return results;
+}
+
+console.log(getTypeNumber(data))
